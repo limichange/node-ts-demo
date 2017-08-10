2a17924d1cb031426c821d790f9af340cb6efa59
limichange <limichange@hotmail.com>
2017-08-10 19:46:18 +0800

    Update

--- src/app.ts
+++ src/app.ts
@@ -2,16 +2,18 @@ import * as Koa from 'koa'
 import * as logger from 'koa-logger'
 import * as BodyParser from 'koa-bodyparser'
 import * as helmet from 'koa-helmet'
+import * as compress from 'koa-compress'
 import passport from './auth'
 import router from './router'
 const app = new Koa()
 
 app
+  .use(compress())
   .use(logger())
   .use(BodyParser())
   .use(helmet())
+  .use(passport.initialize())
   .use(router.routes())
   .use(router.allowedMethods())
-  .use(passport.initialize())
 
 app.listen(3000)
